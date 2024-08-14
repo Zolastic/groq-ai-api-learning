e8ddd0a9379ead21965ce5cf1cd8626185e0d4d8
Zolastic <honhattienlul@gmail.com>
2024-08-14 22:08:35 +0800

    feat: First Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`bg-gradient-to-b from-[#fdfbfb] to-[#ebedee] text-slate-900 ${inter.className}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/groq-component.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React, { useState } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import Groq from "groq-sdk";
+
+const GroqComponent = () => {
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+
+  const groq = new Groq({
+    apiKey: process.env.NEXT_PUBLIC_GROQ_API_KEY,
+    dangerouslyAllowBrowser: true,
+  });
+
+  const handlePrompt = async () => {
+    const chatCompletion = await groq.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content: "what is the fastest car in the world?",
+        },
+      ],
+      model: "llama3-8b-8192",
+      temperature: 1,
+      max_tokens: 1024,
+      top_p: 1,
+      stream: false,
+      stop: null,
+    });
+
+    console.log("Chat completion: ", chatCompletion);
+
+    const response = chatCompletion.choices[0].message.content ?? "No response";
+
+    console.log(response);
+    setResponse(response.replace(/\n/g, "<br />"));
+  };
+
+  return (
+    <div className="flex flex-col gap-y-4 w-full items-center justify-center">
+      <div className="flex gap-x-2 items-center justify-center w-1/5">
+        <Input value={prompt} onChange={(e) => setPrompt(e.target.value)} />
+        <Button onClick={handlePrompt}>Submit</Button>
+      </div>
+      {response && (
+        <div className="flex flex-col gap-x-2 w-4/5">
+          <h1 className="text-lg">Groq Response:</h1>
+          <p>
+            <span dangerouslySetInnerHTML={{ __html: response }} />
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default GroqComponent;
